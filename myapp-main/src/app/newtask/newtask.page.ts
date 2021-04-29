d5af28c4171a914384ae6a06c382eff46cac1314
Leslie Ceja <cejaleslie@gmail.com>
2021-04-29 13:21:55 -0500

    attempt to add custom picker

--- myapp-main/src/app/newtask/newtask.page.ts
+++ myapp-main/src/app/newtask/newtask.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions } from '@ionic/core'
 
 @Component({
   selector: 'app-newtask',
@@ -8,8 +10,41 @@ import { Component, OnInit } from '@angular/core';
 export class NewtaskPage implements OnInit {
 
   myDate: String = new Date().toISOString();
+  reminder='';
+  constructor(private pickerCtrl:PickerController) { }
 
-  constructor() { }
+  async showPicker(){
+    let options :PickerOptions ={
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Done'
+        }
+      ],
+      columns: [
+        {name: 'reminder',
+        options: [
+          {text: '15', value: '15'},
+          {text: '30', value: '30'},
+          {text: '45', value: '45'},
+          {text: '1', value: '1'},
+          {text: '2', value: '2'},
+          {text: '3', value: '3'}
+        ]
+        }
+      ],
+    };
+    let picker = await this.pickerCtrl.create(options);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('Reminder Time');
+      console.log('col:', col);
+      this.reminder = col.options[col.selectedIndex].text;
+    });
+  }
 
   ngOnInit() {
     console.log(this.myDate);
